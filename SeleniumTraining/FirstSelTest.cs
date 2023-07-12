c9ea5a92941542019b552455dddc318514098be5
Sergey Sizov <sergeysizovqa@gmail.com>
2023-07-12 17:49:36 +0300

    test ss

---
+++ SeleniumTraining/FirstSelTest.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumTraining;
+
+public class Tests
+{
+    IWebDriver drv;
+
+    [SetUp]
+    public void Setup()
+    {
+        drv = new ChromeDriver();
+        //drv = new FirefoxDriver();
+        //drv = new EdgeDriver();
+
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        drv.Quit();
+    }
+
+    [Test]
+    public void Test1()
+    {
+        drv.Navigate().GoToUrl("https://google.com");
+        drv.FindElement(By.CssSelector("[name=q]")).SendKeys("Sergey Sizov");
+    }
+}
